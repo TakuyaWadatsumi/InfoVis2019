5cd29512a84a9b1dfb75329dc34e71a3b3dfbd88
弘原海　拓也 <196x121x@istc-a-039.epc.center.kobe-u.ac.jp>
2019-04-17 13:32:54 +0900

    Initial commit

--- W04/main.js
+++ W04/main.js
@@ -9,6 +9,15 @@ function main()
     var aspect = width / height;
     var near = 1;
     var far = 1000;
+
+    var light = new THREE.PointLight( 0xffffff );
+    light.position.set( 1, 1, 1 );
+    scene.add( light );
+
+    var material = new THREE.MeshLambertMaterial({
+	color: 0xff00cc
+    });
+    
     var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
     camera.position.set( 0, 0, 5 );
     scene.add( camera );
@@ -18,7 +27,7 @@ function main()
     document.body.appendChild( renderer.domElement );
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    //var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
