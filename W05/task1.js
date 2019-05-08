84dfdc9d18415595600e4a4c646dbf33bdca6714
TakuyaWadatsumi <wadatsumi@cs26.scitec.kobe-u.ac.jp>
2019-05-08 13:42:42 +0900

    Initial commit

--- W05/task1.js
+++ W05/task1.js
@@ -26,29 +26,63 @@ function main()
         [4,6,7]
 
     ];
-    var v = new Array(8);
-    for( var i = 0; i < 8 ; i++ ){
-        v[i] = new THREE.Vector3().fromArray( vertices[i] );
-    }
-    
-    var id = new Array(12);
-    for(var i=0;i<12;i++){
-        id[i] = faces[i];
-    }
+    var v0 = new THREE.Vector3().fromArray( vertices[0] );
+    var v1 = new THREE.Vector3().fromArray( vertices[1] );
+    var v2 = new THREE.Vector3().fromArray( vertices[2] );
+    var v3 = new THREE.Vector3().fromArray( vertices[3] );
+    var v4 = new THREE.Vector3().fromArray( vertices[4] );
+    var v5 = new THREE.Vector3().fromArray( vertices[5] );
+    var v6 = new THREE.Vector3().fromArray( vertices[6] );
+    var v7 = new THREE.Vector3().fromArray( vertices[7] );
 
-    var f = new Array(12);
-    for(var i=0;i<12;i++){
-        f[i] = new THREE.Face3( id[i][0], id[i][1], id[i][2] );
-    }
+    var id1 = faces[0];
+    var id2 = faces[1];
+    var id3 = faces[2];
+    var id4 = faces[3];
+    var id5 = faces[4];
+    var id6 = faces[5];
+    var id7 = faces[6];
+    var id8 = faces[7];
+    var id9 = faces[8];
+    var id10 = faces[9];
+    var id11 = faces[10];
+    var id12 = faces[11];
+
+    var f0 = new THREE.Face3( id1[0], id1[1], id1[2] );
+    var f1 = new THREE.Face3( id2[0], id2[1], id2[2] );
+    var f2 = new THREE.Face3( id3[0], id3[1], id3[2] );
+    var f3 = new THREE.Face3( id4[0], id4[1], id4[2] );
+    var f4 = new THREE.Face3( id5[0], id5[1], id5[2] );
+    var f5 = new THREE.Face3( id6[0], id6[1], id6[2] );
+    var f6 = new THREE.Face3( id7[0], id7[1], id7[2] );
+    var f7 = new THREE.Face3( id8[0], id8[1], id8[2] );
+    var f8 = new THREE.Face3( id9[0], id9[1], id9[2] );
+    var f9 = new THREE.Face3( id10[0], id10[1], id10[2] );
+    var f10 = new THREE.Face3( id11[0], id11[1], id11[2] );
+    var f11 = new THREE.Face3( id12[0], id12[1], id12[2] );
+
 
     var geometry = new THREE.Geometry();
-    for(var i=0; i<8;i++){
-        geometry.vertices.push( v[i] );
-    }
-    for(var i=0; i<12;i++){
-        geometry.faces.push( f[i] );
-    }
-
+    geometry.vertices.push( v0 );
+    geometry.vertices.push( v1 );
+    geometry.vertices.push( v2 );
+    geometry.vertices.push( v3 );
+    geometry.vertices.push( v4 );
+    geometry.vertices.push( v5 );
+    geometry.vertices.push( v6 );
+    geometry.vertices.push( v7 );
+    geometry.faces.push( f0 );
+    geometry.faces.push( f1 );
+    geometry.faces.push( f2 );
+    geometry.faces.push( f3 );
+    geometry.faces.push( f4 );
+    geometry.faces.push( f5 );
+    geometry.faces.push( f6 );
+    geometry.faces.push( f7 );
+    geometry.faces.push( f8 );
+    geometry.faces.push( f9 );
+    geometry.faces.push( f10 );
+    geometry.faces.push( f11 );
     var width = 500;
     var height = 500;
     var scene = new THREE.Scene();
@@ -66,50 +100,55 @@ function main()
     renderer.setSize( width, height );
     document.body.appendChild( renderer.domElement );
 
+    //var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    //var material = new THREE.MeshBasicMaterial();
+    //material.vertexColors = THREE.FaceColors;
+    //geometry.faces[0].color = new THREE.Color( 1, 0, 0 );
     var material = new THREE.MeshBasicMaterial();
-    material.vertexColors = THREE.FaceColors;
-    for(var i=0; i<12;i++){
-        geometry.faces[i].color=new THREE.Color(0,1,0);
-    }
-
-    //var wireframeGeometry = new THREE.EdgesGeometry(geometry);
-    //var wireframeMaterial = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 2 });
-    //var wireframe = new THREE.LineSegments(wireframeGeometry, wireframeMaterial);
+    material.vertexColors = THREE.VertexColors;
+    geometry.faces[0].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[0].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[0].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[1].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[1].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[1].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[2].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[2].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[2].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[3].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[3].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[3].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[4].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[4].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[4].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[5].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[5].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[5].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[6].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[6].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[6].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[7].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[7].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[7].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[8].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[8].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[8].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[9].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[9].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[9].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[10].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[10].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[10].vertexColors.push(new THREE.Color(0,0,1));
+    geometry.faces[11].vertexColors.push(new THREE.Color(1,0,0));
+    geometry.faces[11].vertexColors.push(new THREE.Color(0,1,0));
+    geometry.faces[11].vertexColors.push(new THREE.Color(0,0,1));
 
     geometry.computeFaceNormals();
     material.side = THREE.DoubleSide
     var cube = new THREE.Mesh( geometry, material );
-    //cube.add(wireframe)
-    //material.wireframe = true;
     scene.add( cube );
 
     loop();
-
-    document.addEventListener( 'mousedown', mouse_down_event );
-	function mouse_down_event( event )
-	{
-        var x_win = event.clientX;
-        var y_win = event.clientY;
-        var vx = renderer.domElement.offsetLeft;
-        var vy = renderer.domElement.offsetTop;
-        var vw = renderer.domElement.width;
-        var vh = renderer.domElement.height;
-        var x_NDC = 2 * ( x_win - vx ) / vw - 1;
-        var y_NDC = -( 2 * ( y_win - vy ) / vh - 1 );
-        var p_NDC = new THREE.Vector3( x_NDC, y_NDC, 1 );
-        var p_wld = p_NDC.unproject( camera );
-        var origin = camera.position;
-        var direction = p_wld.sub(camera.position).normalize();
-        var raycaster = new THREE.Raycaster( origin, direction );
-        var intersects = raycaster.intersectObjects( [cube] );
-        if ( intersects.length > 0 )
-        {
-            intersects[0].face.color.setRGB( 1, 0, 0 );
-            intersects[0].object.geometry.colorsNeedUpdate = true;
-        }
-    }
-
-
 
     function loop()
     {
